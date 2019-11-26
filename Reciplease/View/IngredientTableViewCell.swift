c12ce11ea5b2e70c9aea8d33f1ebb86a2554f759
Léo Nicolas <leoncls3398@gmail.com>
2019-11-26 19:41:56 +0100

    Ajout de Alamofire

---
+++ Reciplease/View/IngredientTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IngredientTableViewCell.swift
+//  Reciplease
+//
+//  Created by Léo NICOLAS on 26/11/2019.
+//  Copyright © 2019 Léo NICOLAS. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Reciplease/View/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  Reciplease
+//
+//  Created by Léo NICOLAS on 26/11/2019.
+//  Copyright © 2019 Léo NICOLAS. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
