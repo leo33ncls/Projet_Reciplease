a392064cd682df0e6de90443b6534d3d29ea581d
Léo Nicolas <leoncls3398@gmail.com>
2019-11-28 21:02:58 +0100

    Ajout de .gitignore

---
+++ Reciplease/Model/RecipeList.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeList.swift
+//  Reciplease
+//
+//  Created by Léo NICOLAS on 26/11/2019.
+//  Copyright © 2019 Léo NICOLAS. All rights reserved.
+//
+
+import Foundation
---
+++ Reciplease/Model/RecipeListService.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeListService.swift
+//  Reciplease
+//
+//  Created by Léo NICOLAS on 26/11/2019.
+//  Copyright © 2019 Léo NICOLAS. All rights reserved.
+//
+
+import Foundation
